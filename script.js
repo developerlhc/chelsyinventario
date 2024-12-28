8746430d510df25eb4361575f7bfe4f3007bc596
Luis Huaman Cortegana <lhuaman@ebizlatin.com>
2024-12-27 22:51:33 -0500

    pull

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+    var products = ["Producto1", "Producto2", "Producto3"];
+    var brands = ["Marca1", "Marca2", "Marca3"];
+    var families = ["Familia1", "Familia2", "Familia3"];
+
+    $("#product").autocomplete({
+        source: products
+    });
+
+    $("#brand").autocomplete({
+        source: brands
+    });
+
+    $("#family").autocomplete({
+        source: families
+    });
+
+    $("#productForm").on("submit", function(event) {
+        event.preventDefault();
+        var product = $("#product").val();
+        var message = "";
+
+        if (products.includes(product)) {
+            message = "Producto actualizado";
+        } else {
+            message = "Producto nuevo";
+            products.push(product);
+        }
+
+        $("#message").text(message);
+    });
+});
